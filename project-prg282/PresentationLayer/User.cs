f43afcdfed4c1c77fe71d671d8e997cb42aad2d0
Francois <Francoisgreeff16@gmail.com>
2021-10-06 16:55:29 +0200

    Added User class and DefineUser Interface, could not complete User class, because I need the datahandler class.

---
+++ project-prg282/PresentationLayer/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_prg282.PresentationLayer
+{
+    class User: iDefineUser
+    {
+        string password, username;
+        public User(){}
+        public User(string password, string username)
+        {
+            this.Password = password;
+            this.Username = username;
+        }
+
+        public string Password { get => password; set => password = value; }
+        public string Username { get => username; set => username = value; }
+
+        public bool ValidateUsername(string username)
+        {
+            // the difference between this and Userexists, is this will only check if the username is found in case of a new user being added
+            //this method is private and is just meant to be used inside the addUser method.
+            return true;
+        }
+
+        public bool UserExists(string Username, string password)// incomplete waiting for dataHandler
+        {
+            bool found = false;
+
+            return false;
+        }
+
+       public void addUser(string Username, string password)// waiting for datahandler
+        {
+
+        }
+    }
+}
