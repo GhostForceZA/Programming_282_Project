0694272ae064ad8005d6880794ad1fe2cbabedf3
Jarod <JarodGeorgeKolbe@gmail.com>
2021-10-11 21:56:24 +0200

    Add filehandler and interfaces

---
+++ project-prg282/DataAccessLayer/IFileControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_prg282.DataAccessLayer
+{
+    interface IFileControl
+    {
+        List<string> ReadTextFile();
+        void WriteToTextFile(string text);
+    }
+}
